4e20718c90131f7fc05c14de398c9f40939a9e0d
qtips <ahm.qadeer@gmail.com>
2016-12-31 23:26:25 +0100

    from tutorial

---
+++ src/main/java/hello/batchimpl/PersonItemProcessor.java
@@ -0,0 +1,25 @@
+package hello.batchimpl;
+
+import hello.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+import java.util.Random;
+
+public class PersonItemProcessor implements ItemProcessor<Person, Person> {
+
+    Logger log = LoggerFactory.getLogger(PersonItemProcessor.class);
+
+    @Override
+    public Person process(final Person person) throws Exception {
+        final String firstName = person.getFirstName().toUpperCase();
+        final String lastName = person.getLastName().toUpperCase();
+
+        final Person transformedPerson = new Person(firstName, lastName);
+
+        log.info(Thread.currentThread().getId()+" Converting (" + person + ") into (" + transformedPerson + ")");
+        Thread.sleep(500 + new Random().nextInt(1500));
+        return transformedPerson;
+    }
+}
